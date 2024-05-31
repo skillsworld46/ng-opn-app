cdf2628199b6fdb05b6800e4d071fc8a6979ff79
skillsworld46@office <skillsworld46@gmail.com>
2024-05-31 17:29:27 +0800

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+    },
+    {
+        component: HomeComponent,
+        path: 'home',
+    }
 ];
