aae440bf4721485a1de5d97c858c40a472c8fcf6
sphing23 <soonphing23@gmail.com>
2025-03-25 09:51:44 +0800

    change primeflex to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 import PrimeUI from 'tailwindcss-primeui';
 module.exports = {
-  content: ["./src/*/.{html,js}"],
+    content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
+    darkMode: ['selector', '[class~="my-app-dark"]'],  
   plugins: [PrimeUI],
 }
-
